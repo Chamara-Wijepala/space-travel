6f9712d49cd1ab603ccab8fcf224898aca453458
Chamara-Wijepala <mr.worf777@gmail.com>
2022-08-19 19:40:00 +0530

    Change gap to row-gap

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -4,16 +4,13 @@ import styled from "styled-components";
 export const Home = styled.main`
   padding-inline: 1.5rem;
   display: grid;
-  gap: 2rem;
+  row-gap: 2rem;
   place-items: center;
   text-align: center;
 
   @media (min-width: 90em) {
     padding-bottom: 8rem;
-    grid-template-columns:
-      1fr
-      repeat(2, minmax(0, 35rem))
-      1fr;
+    grid-template-columns: 1fr repeat(2, minmax(0, 35rem)) 1fr;
     align-items: end;
 
     > :first-child {

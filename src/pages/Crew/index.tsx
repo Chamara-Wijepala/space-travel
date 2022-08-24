50c348dbdd3313beee195ef8ade3a494fca33310
Chamara-Wijepala <mr.worf777@gmail.com>
2022-08-24 16:06:27 +0530

    Add tab switching functionality to crew page

--- src/pages/Crew/index.tsx
+++ src/pages/Crew/index.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import clsx from "clsx";
+
+import handleTabChange from "utils/handleTabChange";
 
 import commanderImage from "assets/crew/image-douglas-hurley.webp";
 import specialistImage from "assets/crew/image-mark-shuttleworth.webp";
@@ -8,6 +11,7 @@ import engineerImage from "assets/crew/image-anousheh-ansari.webp";
 import * as S from "./style";
 
 interface CrewMember {
+  id: number;
   name: string;
   image?: string;
   role: string;
@@ -21,11 +25,31 @@ interface IProps {
 function index({ data }: IProps) {
   const [commander, specialist, pilot, engineer] = data;
   const [currentCrewMember, setCurrentCrewMember] = useState(commander);
+  const [currentTab, setCurrentTab] = useState(0);
 
   commander.image = commanderImage;
   specialist.image = specialistImage;
   pilot.image = pilotImage;
   engineer.image = engineerImage;
+
+  useEffect(() => {
+    switch (currentTab) {
+      case 0:
+        setCurrentCrewMember(commander);
+        break;
+      case 1:
+        setCurrentCrewMember(specialist);
+        break;
+      case 2:
+        setCurrentCrewMember(pilot);
+        break;
+      case 3:
+        setCurrentCrewMember(engineer);
+        break;
+      default:
+        break;
+    }
+  }, [currentTab]);
 
   return (
     <S.Main className="grid-container">
@@ -35,19 +59,20 @@ function index({ data }: IProps) {
 
       <img src={currentCrewMember.image} alt={`${currentCrewMember.name}`} />
 
-      <S.TabPanel>
-        <button type="button">
-          <span className="sr-only">the commander</span>
-        </button>
-        <button type="button">
-          <span className="sr-only">the mission specialist</span>
-        </button>
-        <button type="button">
-          <span className="sr-only">the pilot</span>
-        </button>
-        <button type="button">
-          <span className="sr-only">the flight engineer</span>
-        </button>
+      <S.TabPanel className="dot-indicators">
+        {data.map((crewMember) => (
+          <button
+            type="button"
+            key={crewMember.id}
+            data-index={crewMember.id}
+            onClick={(e) => {
+              handleTabChange(e, setCurrentTab);
+            }}
+            className={clsx(currentTab === crewMember.id && "active")}
+          >
+            <span className="sr-only">{`The ${crewMember.role}`}</span>
+          </button>
+        ))}
       </S.TabPanel>
 
       <S.Article>
